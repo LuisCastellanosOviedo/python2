fda82363ba858c5463514b1a5750c6fa0c5f0514
Luis Alejandro <luis@Luiss-MacBook-Air.local>
2021-09-01 20:47:36 -0500

    new folder order and start new python course Day 1

---
+++ my-python-project/day1/main.py
@@ -0,0 +1,39 @@
+print("Hello")
+print('Hello')
+print("print('what to print')")
+
+
+# code manipulation
+print("#######################################################\n")
+print("Hello World \n")
+print("Hello"+" Luis")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+print("#######################################################\n")
+
+#print("Hello "+input(" What is your name: "))
+
+#print(len(input("Say a word: ")))
+
+#name = input("What is your name")
+#print(name)
+
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print(f"a: {a} and b: {b}")
+
+
+
+
+
+
