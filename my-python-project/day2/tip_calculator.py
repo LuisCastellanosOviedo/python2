478bc23dcfd54f8ea03ce0707b571db4f1a0977e
Luis Alejandro <luis@Luiss-MacBook-Air.local>
2021-09-05 20:04:05 -0500

    complete day 2 , start day 3 if else stuff

--- my-python-project/day2/tip_calculator.py
+++ my-python-project/day2/tip_calculator.py
@@ -4,5 +4,15 @@ tip = int(input("How much tip do you want to give ? 10 , 12 , 15 ??"))
 people = int(input("How many people to split the bill ? "))
 
 
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+
+bill_per_person = total_bill/people
+final_amout = round(bill_per_person, 2)
+final_amout = "{:.2f}".format(final_amout)
+print(f" Each person should pay:  ${final_amout} ")
+
+
 
 
