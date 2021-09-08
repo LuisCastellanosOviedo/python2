c18f564f253ee119dc1c9053cee6e6cadcb13cc7
Luis Alejandro <luis@Luiss-MacBook-Air.local>
2021-09-07 21:08:04 -0500

    random.choice hahaha easy peasy

--- my-python-project/day4_randomisation_and_python_list/russian_roulette_bill.py
+++ my-python-project/day4_randomisation_and_python_list/russian_roulette_bill.py
@@ -3,4 +3,8 @@ import random
 
 names_string = input("Give me everybody's names, separated by a comma.")
 list_of_people = names_string.split(",")
-print(f"{list_of_people[random.randint(0,len(list_of_people))]} should pay the bill MUAHAHAHAHAHA")
+print(f"{list_of_people[random.randint(0,len(list_of_people)-1)]} should pay the bill MUAHAHAHAHAHA")
+
+
+print("or easy way ")
+print(random.choice(list_of_people))
