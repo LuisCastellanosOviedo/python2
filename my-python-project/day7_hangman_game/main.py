703f27ceabe92250048b54f9c6c62a19178f5dde
Luis Alejandro <luis@Luiss-MacBook-Air.local>
2021-09-21 20:31:49 -0500

    hangman game completed :) and fisrt example for functions with parameters

--- my-python-project/day7_hangman_game/main.py
+++ my-python-project/day7_hangman_game/main.py
@@ -1,6 +1,9 @@
 import random
+from replit import clear
+from hangman_art import logo, stages
+from hangman_words import word_list
 
-word_list = ["hook", "baboon", "camel"]
+print(logo)
 
 chosen_word = random.choice(word_list)
 print(print(f" here is the word {chosen_word}"))
@@ -15,14 +18,30 @@ for _ in range(len(chosen_word)):
     display += "_"
 print(display)
 
-guess = input("Guess a letter: ").lower()
+end_of_game = False
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-        print("Right")
-    else:
-        print("Wrong bitch ! ")
+count = 5
+while not end_of_game:
 
-print(display)
+    guess = input("Guess a letter: ").lower()
+    clear()
+
+    letterPresent = False
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+            letterPresent = True
+
+    if not letterPresent:
+        print(stages[count])
+        count -= 1
+
+    print(display)
+    if count < 0:
+        print("You !!! looserrrr ")
+        end_of_game = True
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win ! ")
---
+++ my-python-project/day8_functions_with_inputs/main.py
@@ -0,0 +1,16 @@
+def greet():
+    print("Hello")
+    print("how do you do?")
+    print("isn't the weather nice today ?")
+
+
+greet()
+
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"how do you do {name}?")
+
+
+greet_with_name("Luis")
+greet_with_name(35)
